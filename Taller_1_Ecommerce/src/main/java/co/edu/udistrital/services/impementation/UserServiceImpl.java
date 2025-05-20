0ac875782600ce89cc5cffe9568d864396a6803c
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-05-20 17:39:31 -0500

    .

--- Taller_1_Ecommerce/src/main/java/co/edu/udistrital/services/impementation/UserServiceImpl.java
+++ Taller_1_Ecommerce/src/main/java/co/edu/udistrital/services/impementation/UserServiceImpl.java
@@ -2,10 +2,17 @@ package co.edu.udistrital.services.impementation;
 
 import co.edu.udistrital.data.UsersDTO;
 import co.edu.udistrital.services.IRestServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements IRestServices<UsersDTO> {
+
+    private final RestTemplate restTemplate;
 
     @Override
     public List<UsersDTO> getAll() {
