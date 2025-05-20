00607f60ad17e3aa8dc357d73e4b66f989d6e75c
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-05-20 12:35:53 -0500

    Inicializacion proyecto

---
+++ Taller_1_Ecommerce/src/main/java/co/edu/udistrital/taller_1_ecommerce/Taller1EcommerceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.taller_1_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1EcommerceApplication.class, args);
+    }
+
+}
---
+++ Taller_1_Ecommerce/src/test/java/co/edu/udistrital/taller_1_ecommerce/Taller1EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.taller_1_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
