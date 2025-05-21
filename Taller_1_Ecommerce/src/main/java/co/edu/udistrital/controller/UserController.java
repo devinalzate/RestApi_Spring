00f3c89aa7961ba05441abd018865a597ae013cf
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-05-21 15:01:06 -0500

    restmapping user

--- Taller_1_Ecommerce/src/main/java/co/edu/udistrital/controller/UserController.java
+++ Taller_1_Ecommerce/src/main/java/co/edu/udistrital/controller/UserController.java
@@ -5,9 +5,7 @@ import co.edu.udistrital.services.IRestServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,26 @@ public class UserController {
     public ResponseEntity<?> getAllUsers() {
         return ResponseEntity.ok(userService.getAll());
     }
+
+    @PostMapping
+    public ResponseEntity<?> AddNewUser(UsersDTO usersDTO) {
+        return ResponseEntity.ok(userService.AddNew(usersDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> UpdateUser(@PathVariable int id, UsersDTO usersDTO) {
+        return ResponseEntity.ok(userService.Update(id,usersDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable int id) {
+        userService.Delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
