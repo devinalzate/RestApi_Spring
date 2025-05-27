c4010242f69cd78503640417fec6e38036c75c02
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-05-27 11:28:52 -0500

    ya funciona acceder a los productos

--- Taller_1_Ecommerce/src/main/java/co/edu/udistrital/controller/ProductController.java
+++ Taller_1_Ecommerce/src/main/java/co/edu/udistrital/controller/ProductController.java
@@ -3,19 +3,22 @@ package co.edu.udistrital.controller;
 import co.edu.udistrital.data.ProductDTO;
 import co.edu.udistrital.services.impementation.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
     private ProductServiceImpl productService;
 
     @GetMapping
-    public List<ProductDTO> getAllProducts() {
-        return productService.getAll();
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", productService.getAll());
+        return "products";
     }
 
     @GetMapping("/{id}")
